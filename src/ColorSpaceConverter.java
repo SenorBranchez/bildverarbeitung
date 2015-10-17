2c9f793c83263c80c0942ae78bce9ee16036ff7f
Dominik Soellinger <dominik.soellinger@at.redbullmediahouse.com>
2015-10-17 11:47:17 +0200

    YSV and HSV color space conversion

---
+++ src/ColorSpaceConverter.java
@@ -0,0 +1,65 @@
+import java.awt.*;
+
+public class ColorSpaceConverter {
+
+    static int[] convertRgb2Yuv( int[] srcImage, int width, int height) {
+
+        int numOfPixels = width*height;
+        int[] destImage = new int[numOfPixels*3];
+
+        int r;
+        int g;
+        int b;
+
+        int y;
+        int u;
+        int v;
+
+        for( int i = 0; i < numOfPixels; i+=3) {
+
+            r = srcImage[i];
+            g = srcImage[i+1];
+            b = srcImage[i+2];
+
+            y = (int) Math.round( 0.299*r + 0.587*g + 0.114*b );
+            u = (int) Math.round( 0.492 * ( b-y) );
+            v = (int) Math.round( 0.299 * (r-y) );
+
+            destImage[i]  = y;
+            destImage[i+1]  = u;
+            destImage[i+2]  = v;
+        }
+
+        return destImage;
+    }
+
+    static float[] convertRgb2Hsv( int[] srcImage, int width, int height) {
+
+        int numOfPixels = width*height;
+        float[] destImage = new float[numOfPixels*4];
+
+        int r;
+        int g;
+        int b;
+
+        float[] hsv = new float[3];
+
+        for( int i = 0; i < numOfPixels; i+=3) {
+
+            r = srcImage[i];
+            g = srcImage[i+1];
+            b = srcImage[i+2];
+
+            // Note: HSB is the same as HSV
+            Color.RGBtoHSB(r,g,b,hsv);
+
+            destImage[i]  = hsv[0];
+            destImage[i+1]  = hsv[1];
+            destImage[i+2]  = hsv[2];
+
+
+        }
+
+        return null;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String [] args) {
+
+        BufferedImage srcImage;
+        File output;
+        String filename = "test.jpg";
+        int[] srcImageData;
+        int[] destImageYUVData;
+        float[] destImageHSVData;
+        int width;
+        int height;
+
+        try {
+
+            srcImage = ImageIO.read( new File(filename) );
+            output = new File("out.jpeg");
+
+            width = srcImage.getWidth();
+            height = srcImage.getHeight();
+
+            srcImageData = srcImage.getRGB(0, 0, width, height, null, 0, width);
+
+            destImageYUVData = ColorSpaceConverter.convertRgb2Yuv(srcImageData, width, height);
+            destImageHSVData = ColorSpaceConverter.convertRgb2Hsv(srcImageData, width, height);
+
+
+
+        } catch (IOException e) {
+            System.err.print("File " + filename + " not found");
+        }
+    }
+}
